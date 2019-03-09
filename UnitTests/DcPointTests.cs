57357a4a1579cadf260378c96ad8ce0c9c2c84bf
ArtemChalov <chalov81@mail.ru>
2019-03-09 12:29:16 +0500

    Added DcPoint Unit tests.

---
+++ UnitTests/DcPointTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using DraftCanvas;
+using DraftCanvas.Servicies;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DcPointTests
+    {
+        [TestMethod]
+        public void PoinsAreEquals()
+        {
+            DcPoint point1 = new DcPoint(new Point(100, 300), PointHash.CreateHash(1, 2), 1);
+            DcPoint point2 = new DcPoint(new Point(100, 300), PointHash.CreateHash(2, 3), 1);
+
+            Assert.IsTrue(point1.Equals(point2));
+        }
+
+        [TestMethod]
+        public void PoinsAreNotEquals()
+        {
+            DcPoint point1 = new DcPoint(new Point(100, 250), PointHash.CreateHash(1, 2), 1);
+            DcPoint point2 = new DcPoint(new Point(100, 300), PointHash.CreateHash(2, 2), 1);
+
+            Assert.IsFalse(point1.Equals(point2));
+        }
+    }
+}
